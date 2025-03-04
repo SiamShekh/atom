c47fc12981f6bdb086f486a5215db4b358e2cca6
Md. Siam Shekh <siam62349@gmail.com>
2025-03-04 19:26:09 +0600

    changed

--- server/src/environment.ts
+++
@@ -1,7 +0,0 @@
-const Env = {
-    bot_token: "7657390165:AAGifjGcjqThbum_Lf4Lra3b5HXFIGlPqEw",
-    secret: "abc",
-    uri: "mongodb+srv://siam_dev:Siam1234@cluster1.4byih.mongodb.net/AAAAATOM"
-};
-
-export default Env;
